802ffb64d8a43fd2a7e9c6b35661005ee45c6fcc
jon <phompasit2323@gmail.com>
2024-08-08 16:19:01 +0700

    sa1

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 const api = require('./routes/router')
 const corsOptions = {
-  origin: ["office-zeta.vercel.app"],
+  origin: ["http://localhost:8001"],
   credentials: true,
   optionSuccessStatus: 200,
 };
