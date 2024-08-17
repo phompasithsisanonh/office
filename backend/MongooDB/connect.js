aa5f170f554a039844018d4b52f75c726eb01f6c
jon <phompasit2323@gmail.com>
2024-08-17 08:24:33 +0700

    b1

--- backend/MongooDB/connect.js
+++ backend/MongooDB/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const options = {
   serverSelectionTimeoutMS: 5000,
   autoIndex: false,
@@ -10,8 +10,16 @@ const options = {
 
 const connectDB = async (url) => {
   try {
-    await mongoose.connect(url, options);
-    console.log("DB is connected");
+    const client = new MongoClient(url, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    await  await client.connect() ;
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
     console.error("Error connecting to DB:", err);
     process.exit(1); // Exit the process if the database connection fails
